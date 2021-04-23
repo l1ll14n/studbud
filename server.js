db9e0b6c396655d17f2de8ea2fc458cee0dc987d
l1ll14n <70677567+l1ll14n@users.noreply.github.com>
2021-04-23 12:47:33 +1000

    added public folder, style.css, index.html, script.js, and server.js and set it up

---
+++ server.js
@@ -0,0 +1,18 @@
+// import express package
+const express = require('express');
+
+// initialise express framework
+const app = express();
+
+// serve static files from the public folder
+app.use(express.static('public'));
+
+// serve the index file for the root ("/") path
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+   })
+
+// start server, listen for incoming traffic and log message to console
+let server = app.listen(8888, function(){
+    console.log("App server is running on port 8888");
+   });
